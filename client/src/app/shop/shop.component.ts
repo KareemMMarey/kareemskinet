f6dc78ff2d38e1131fe675ec3486567fdbf4d713
KareemMMarey <kareem.m.marey@gmail.com>
2020-05-29 07:14:13 +0300

    working with products and details - routers -lazyloading

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -89,8 +89,11 @@ export class ShopComponent implements OnInit {
   }
 
   onPageChanged(event: any) {
-    this.shopParams.pageNumber = event;
-    this.getProducts();
+
+    if (this.shopParams.pageNumber !== event){
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
   }
   onSearch() {
     this.shopParams.search = this.searchTerm.nativeElement.value;
