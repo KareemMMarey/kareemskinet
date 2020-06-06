6a2ec7f1d16932f39508acc1140fbae78c365fba
KareemMMarey <kareem.m.marey@gmail.com>
2020-06-06 13:41:45 +0300

    error handling section

---
+++ client/src/app/core/test-error/test-error.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.scss']
+})
+export class TestErrorComponent implements OnInit {
+  baseUrl = environment.apiUrl;
+  validationErrors: any;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+  get404Error(){
+    this.http.get(this.baseUrl + 'products/42').subscribe(res => {
+     console.log(res);
+    },
+    err =>{
+      console.log(err);
+    });
+  }
+  get400validationError(){
+    this.http.get(this.baseUrl + 'products/fortitow').subscribe(res => {
+     console.log(res);
+    },
+    err =>{
+      this.validationErrors = err.errors;
+      console.log(err);
+    });
+  }
+  get500Error(){
+    this.http.get(this.baseUrl + 'buggy/servererror').subscribe(res => {
+     console.log(res);
+    },
+    err =>{
+      console.log(err);
+    });
+  }
+  get400Error(){
+    this.http.get(this.baseUrl + 'buggy/badrequest').subscribe(res => {
+     console.log(res);
+    },
+    err =>{
+      console.log(err);
+    });
+  }
+
+}
